8766d5ffa9773df1aebf2ff8ce0ccbd9782676bf
kiendv-prox <kiendv@proxglobal.com>
2025-06-24 15:07:52 +0700

    Update package
    Update package

--- Package.swift
+++ Package.swift
@@ -5,16 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "MediationAds",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MediationAds",
             targets: ["MediationAds"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", revision: "11.14.0"),
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", revision: "6.17.0"),
+        .package(url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector", revision: "6.16.2"),
+        .package(url: "https://github.com/Kitura/Swift-JWT", revision: "3.6.201")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MediationAds"),
+            name: "MediationAds",
+            dependencies: [
+                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseABTesting", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
+                .product(name: "AppsFlyerLib", package: "AppsFlyerFramework"),
+                .product(name: "PurchaseConnector", package: "appsflyer-apple-purchase-connector"),
+                .product(name: "SwiftJWT", package: "swift-jwt")
+            ]
+        ),
     ]
 )
